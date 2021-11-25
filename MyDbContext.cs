87f08c8567cc26a00acdcd87f79bac6482434085
unknown <chenxinxin@tcdri.lan>
2021-11-25 16:20:18 +0800

    20211125 16:20

--- MyDbContext.cs
+++ MyDbContext.cs
@@ -61,8 +61,8 @@ namespace MySConn
         #endregion
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //string sql = "Server=10.1.5.61;Port=3306;User=chenxinxin;Password=123456;Database=revit_cal";
-            string sql = "Server=127.0.0.1;Port=3306;User=root;Password=123456;Database=revit_cal";
+            string sql = "Server=10.1.5.61;Port=3306;User=chenxinxin;Password=123456;Database=revit_cal";
+            //string sql = "Server=127.0.0.1;Port=3306;User=root;Password=123456;Database=revit_cal";
             optionsBuilder.UseMySQL(sql);
         }
 
@@ -74,13 +74,13 @@ namespace MySConn
         public double get_ksai_easyway(List<double> keys, List<double> values)//keys保存每条记录的get_distance数值，values保存ksai数值
         {
             List<double> tmpkeys = new List<double>();
-            keys.ForEach(v => tmpkeys.Add(v));
+            keys.ForEach(v => tmpkeys.Add(Math.Abs(v)));
             tmpkeys.Sort();//sort 会改变顺序
             double minkeys = tmpkeys[0];//保存最近距离值，排序可以重复
             List<int> indexs = new List<int>();
             for (int i = 0; i < keys.Count; i++)
             {
-                if (keys[i] == minkeys)
+                if (Math.Abs(keys[i]) == minkeys)
                 {
                     indexs.Add(i);//index保存所有相同距离数值,有则返回平均值，无则返回最小值
                 }
